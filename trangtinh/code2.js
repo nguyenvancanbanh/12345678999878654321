ad22018124c92f7b4e2498ec48000171ebf3d384
nguyenvancanbanh <nguyenvancanbanh@gmail.com>
2025-10-27 19:23:40 +0700

    Update code2.js

--- trangtinh/code2.js
+++ trangtinh/code2.js
@@ -1,4 +1,1 @@
-const SCRIPT_URL = "const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzUqQAOVFdYbuzTgBy4KOqDMeo4BsgKnf7CV7vQTRVLaFRYSZXuI_LsPm9Yo-urWnfB/exec";
-
-
-
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzUqQAOVFdYbuzTgBy4KOqDMeo4BsgKnf7CV7vQTRVLaFRYSZXuI_LsPm9Yo-urWnfB/exec";
